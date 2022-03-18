f832bca03a385ad5ecef345c0834588c60efa205
Prakhar Khandelwal <90647055+pk-op-06@users.noreply.github.com>
2022-03-19 04:59:50 +0530

    Updated Comment in startup,sh

--- startup.sh
+++ startup.sh
@@ -5,6 +5,6 @@ Author: Prakhar Khandelwal
 Purpose: To run vpn.sh script on user login on a new terminal
 INFO
 
-# kill all previous openvpns when you logout.
+# kill all previous openvpns when you login.
 sudo killall openvpn
 gnome-terminal -e "vpn.sh"
